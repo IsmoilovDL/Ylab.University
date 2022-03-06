4fc494187036aac01a08d5b0e58a77e08e95e741
311 <ismoilov92@gmail.com>
2022-03-07 00:17:50 +0300

    добавлен продолжение игры при победе

--- Lesson-2/src/game/Start.java
+++ Lesson-2/src/game/Start.java
@@ -19,7 +19,6 @@ public class Start {
         gameTable game =new gameTable();
         game.newGame();
         System.out.println(game.getTableXO());
-        System.out.println(game.searchWinner("X"));
 
         while (true){
             System.out.println(player1+ " введи номер строки от 1 до 3");
@@ -28,15 +27,18 @@ public class Start {
             int column=scannner.nextInt();
             game.setPosition(row-1,column-1, "X");
             System.out.println(game.getTableXO());
+
             if(game.searchWinner("X")){
                 System.out.println("Победил игрок "+ player1);
 
-                System.out.println("Хотите продолжать игру? y-n");
-                String answer = scannner.nextLine();
-                if(answer.equals("y")){
+                System.out.println("Хотите продолжать игру? 0-да 1-нет");
+                int answer=scannner.nextInt();
+                if(answer==0){
+                    game.newGame();
+                    System.out.println(game.getTableXO());
                     continue;
                 }
-                else if (answer.equals("n")){
+                else if (answer==1){
                     break;
                 }
             }
@@ -49,6 +51,16 @@ public class Start {
             System.out.println(game.getTableXO());
             if(game.searchWinner("0")){
                 System.out.println("Победил игрок "+ player2);
+                System.out.println("Хотите продолжать игру? 0-да 1-нет");
+                int answer=scannner.nextInt();
+                if(answer==0){
+                    game.newGame();
+                    System.out.println(game.getTableXO());
+                    continue;
+                }
+                else if (answer==1){
+                    break;
+                }
             }
         }
 
